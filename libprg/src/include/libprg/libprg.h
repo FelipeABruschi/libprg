82a886aa88efd6c1240bcc8ffe8513e59d36d47a
FelipeABruschi <110953697+FelipeABruschi@users.noreply.github.com>
2024-03-10 12:46:31 -0300

    Update libprg.h

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -8,6 +8,18 @@ typedef struct {
 
 typedef enum { SUM, SUB } operation_t;
 
+typedef struct {
+    int *valores;
+    int tamanho;
+    int qtd_elementos;
+} Lista;
+
+void criar_lista(Lista *lista, int tamanho);
+void imprimir(Lista *lista);
+void inserir_nao_ord(Lista *lista, int x);
+int busca_linear(Lista *lista, int x);
+void remover_nao_ord(Lista *lista, int x);
+
 result_t sub(double a, double b);
 result_t sum(double a, double b);
 result_t compute(double a, double b, operation_t op);
