57a858539fb28c86eab46a600216f825b48100b3
FelipeABruschi <felipeaugbr@outlook.com>
2024-03-25 14:34:11 -0300

    funções fila

--- libprg/src/libprg/fila_circular.c
+++ libprg/src/libprg/fila_circular.c
@@ -30,3 +30,68 @@ Fila *criar_fila(int capacidade)
 
     return fila;
 }
+
+void imprimir_fila(Fila *fila)
+{
+    for(int i = 0; i < fila->tamanho; i++)
+        printf("%d ", fila->valores[i]);
+    printf("\n");
+}
+
+void enfileirar(Fila *fila, int x)
+{
+    fila->valores[fila->fim] = x;
+    fila->fim++;
+
+    fila->tamanho++;
+}
+
+void desenfileirar(Fila *fila)
+{
+    if(fila->inicio == fila->fim)
+    {
+        printf("fila já está vazia");
+        exit(1);
+    }
+
+    fila->inicio++;
+
+    fila->tamanho--;
+}
+
+int inicio(Fila *fila)
+{
+    return fila->valores[fila->inicio];
+    //retorna o valor que esta no inicio da fila
+}
+
+int fim(Fila *fila)
+{
+    return fila->valores[fila->fim];
+    //retorna o valor que esta no fim da fila
+}
+
+int tamanho(Fila *fila)
+{
+    return fila->tamanho;
+    //retorna o total de elementos da fila
+}
+
+bool cheia(Fila *fila)
+{
+    if(fila->tamanho == fila->capacidade)
+        return true;
+    else
+        return false;
+}
+
+bool vazia(Fila *fila)
+{
+    if(fila->tamanho == 0)
+        return true;
+    else
+        return false;
+}
+
+
+
